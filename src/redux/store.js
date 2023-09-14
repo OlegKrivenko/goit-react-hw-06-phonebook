118bd010d8321fae88f3a60040b6ef24d4f76990
OlegKrivenko <krivenko.olegus@gmail.com>
2023-09-14 23:02:04 +0300

    createSlice has added to contacts

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,27 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { contactsReducer, filterContactsReducer } from './reducer';
+import {
+  persistStore,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
 
 export const store = configureStore({
   reducer: {
     contacts: contactsReducer,
     filter: filterContactsReducer,
   },
+  middleware(getDefaultMiddleware) {
+    return getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    });
+  },
 });
+
+export const persistor = persistStore(store);
